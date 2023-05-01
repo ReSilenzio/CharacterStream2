c5c6a22cb5ecb94ceb6f186a1f71c1bd86197a30
methus <methus.dumr@kmutt.ac.th>
2023-05-02 04:25:15 +0700

    first commit

---
+++ src/characterstream2/LAB4.java
@@ -0,0 +1,26 @@
+package characterstream2;
+
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LAB4 {
+
+    public static void main(String[] args) {
+        try {
+            InputStreamReader key = new InputStreamReader(System.in);
+            BufferedReader input = new BufferedReader(key);
+            System.out.print("Enter your name: ");
+            String name = input.readLine();
+            System.out.println("Hello " + name);
+            System.out.print("Enter your weight: ");
+            String w = input.readLine();
+            System.out.print("Enter your height: ");
+            String h = input.readLine();
+            System.out.println(Double.parseDouble(h) * Double.parseDouble(h));
+        } catch (IOException ex) {
+            Logger.getLogger(LAB4.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
